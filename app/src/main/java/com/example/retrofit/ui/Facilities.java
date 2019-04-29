4dca152c8c151302e585e94027c6dd9ded69fa6a
javie <javierrrr.15@gmail.com>
2019-04-29 17:57:51 -0500

    csm

---
+++ app/src/main/java/com/example/retrofit/ui/Facilities.java
@@ -0,0 +1,15 @@
+package com.example.retrofit.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.retrofit.R;
+
+
+public class Facilities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facilities);
+    }
+}
